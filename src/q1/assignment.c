ecae5ebd35f77f094b5bff5577cd758c83c1cec8
Cormac Buckley <cbuckley97@gmail.com>
2017-09-21 15:47:08 +0100

    Question 1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+	int a = 0;
+	int* b = 0;
+	long c = 0;
+	double * d = 0;
+	char** e = 0;
+
+	printf("Int: %d \n", sizeof(a));
+	printf("Int*: %d \n", sizeof(b));
+	printf("Long: %d \n", sizeof(c));
+	printf("Double *: %d \n", sizeof(d));
+	printf("Char**: %d \n", sizeof(e));
+
 }
